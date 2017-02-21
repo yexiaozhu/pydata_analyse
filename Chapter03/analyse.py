dd92cce7df8d8280698fb2a1e98271c3407dca56
557703988@139.com <557703988@139.com>
2017-02-21 01:51:58 -0800

    Chapter: 04

---
+++ Chapter03/analyse.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# coding=utf-8
+# author=yexiaozhu
+from numpy.random import random
+
+a = 5
+# print a
+
+import numpy as np
+data = {i : random() for i in range(7)}
+# print data
+an_apple = 27
+an_example = 42
